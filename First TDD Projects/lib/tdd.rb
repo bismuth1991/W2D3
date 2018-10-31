eb5a5e44d9cbcbbb9be85596024dd9fd18d267cb
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-10-31 14:51:51 -0700

    Write spec for #two_sum, #my_transpose, and #stock_picker

--- First TDD Projects/lib/tdd.rb
+++ First TDD Projects/lib/tdd.rb
@@ -1,10 +1,55 @@
-
 def my_uniq(array)
   raise ArgumentError unless array.class == Array
 
   answer = []
   array.each do |ele|
     answer << ele unless answer.include?(ele)
+  end
+
+  answer
+end
+
+def two_sum(array)
+  raise ArgumentError unless array.class == Array
+
+  answer = []
+  array.each_with_index do |el1, i1|
+    array.each_with_index do |el2, i2|
+      if i2 > i1 && el1 + el2 == 0
+        answer << [i1, i2]
+      end
+    end
+  end
+
+  answer
+end
+
+def my_transpose(array)
+  answer = []
+
+  array[0].length.times do |i1|
+    new_row = []
+    array.length.times do |i2|
+      new_row << array[i2][i1]
+    end
+
+    answer << new_row
+  end
+
+  answer
+end
+
+def stock_picker(arr)
+  difference = 0
+  answer = nil
+
+  arr.each_with_index do |price1, day1|
+    arr.each_with_index do |price2, day2|
+      if day1 < day2 && ((price2 - price1) > difference )
+        difference = price2 - price1
+        answer = [day1, day2]
+      end
+    end
   end
 
   answer
--- First TDD Projects/spec/tdd_spec.rb
+++ First TDD Projects/spec/tdd_spec.rb
@@ -1,30 +1,81 @@
 require "tdd"
-# require "rspec"
-# require "spec_helper"
 
-# RSpec.describe Array do
+RSpec.describe "#my_uniq" do
+  subject(:my_arr) { [1, 1, 3, 5, 3, 4] }
 
-  RSpec.describe "#my_uniq" do
-    subject(:my_arr) { [1, 1, 3, 5, 3, 4] }
-
-    context "when the argument is not an array" do
-      subject(:my_not_arr) { "blahhhh" }
-      it "throws an ArgumentError" do
-        expect { my_uniq(my_not_arr) }.to raise_error(ArgumentError)
-      end
+  context "when the argument is not an array" do
+    subject(:my_not_arr) { "blahhhh" }
+    it "throws an ArgumentError" do
+      expect { my_uniq(my_not_arr) }.to raise_error(ArgumentError)
     end
-
-    context "when the argument is an array" do
-      it "only appears once in the array" do
-        expect(my_uniq(my_arr)).to eq([1, 3, 5, 4])
-      end
-
-      it "doesn't mutate the original array" do
-        my_uniq(my_arr)
-        expect(my_arr).to eq([1, 1, 3, 5, 3, 4])
-      end
-    end
-
   end
 
-# end
+  context "when the argument is an array" do
+    it "only appears once in the array" do
+      expect(my_uniq(my_arr)).to eq([1, 3, 5, 4])
+    end
+
+    it "doesn't mutate the original array" do
+      my_uniq(my_arr)
+      expect(my_arr).to eq([1, 1, 3, 5, 3, 4])
+    end
+  end
+
+end
+
+
+RSpec.describe "#two_sum" do
+  subject(:arr) { [-1, 0, 2, -2, 1] }
+
+  context "when the argument is an array" do
+    it "finds all indices where the sum is zero" do
+      expect(two_sum(arr)).to eq([[0, 4], [2, 3]])
+    end
+
+    it "doesn't repeat the same pair of indices" do
+      expect(two_sum(arr)).to_not eq([[0, 4], [4, 0], [2, 3], [3, 2]])
+    end
+
+    it "starts with the smallest indices" do
+      expect(two_sum(arr)).to_not eq([[2, 3], [0, 4]])
+    end
+  end
+
+  context "when the argument is not an array" do
+    it "raises an ArgumentError when the argument is not an array" do
+      expect { two_sum(100) }.to raise_error(ArgumentError)
+    end
+  end
+end
+
+RSpec.describe "#my_transpose" do
+  subject(:arr) {
+    [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]
+    ]
+  }
+
+  it "correctly transposes array" do
+    expect(my_transpose(arr)).to eq(arr.transpose)
+  end
+
+  it "does not flatten the array" do
+    expect(my_transpose(arr).length).to eq(arr.length)
+  end
+
+end
+
+RSpec.describe "stock_picker" do
+  subject(:stock_prices) { [100, 3, 12, 20, 1] }
+
+  it "doesn't sell before it buys" do
+    expect(stock_picker(stock_prices)).to_not eq([0, 4])
+  end
+
+  it "correctly picks the two days" do
+    expect(stock_picker(stock_prices)).to eq([1, 3])
+  end
+
+end
